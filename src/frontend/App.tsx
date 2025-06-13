ec3388c8e8d3cf3c9a22c85c5f41120ccded87ac
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-13 04:49:10 -0500

    added good styling

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -2,8 +2,22 @@
 function App() {
   return (
     <>
-    <h1 className=""
-    >leaft</h1>
+
+    <div>
+
+    </div>
+
+    <h1 className=" text-8xl/normal italic font-serif font-extrabold
+    text-shadow-lg/100  text-shadow-green-900
+    ml-5"
+    >Leaf</h1>
+
+    <h2 className="text-2xl/normal italic font-serif font-semibold
+    text-shadow-lg/20 text-shadow
+    ml-2 mt-4"
+    >~ <span id="breeze">Breeeze</span> Through The Classics ~  </h2>
+
+
     </>
   )
 };
