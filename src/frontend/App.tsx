e2b76921697060ed1644bfc7aef867f2d80b223a
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-13 15:21:33 -0500

    adding nav bar

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,25 +1,23 @@
 
 function App() {
+
   return (
     <>
 
     <div>
+      <h1 className=" text-8xl/normal italic font-serif font-extrabold
+      text-shadow-lg/100  text-shadow-green-900
+      ml-5" >Leaf</h1>
 
+
+      <h2 className="text-5xl/normal italic font-serif font-semibold
+      text-shadow-lg/20 text-shadow
+      ml-2 mt-4"
+      >~ <span id="breeze">Breeeze</span> Through The Classics ~  </h2>
     </div>
-
-    <h1 className=" text-8xl/normal italic font-serif font-extrabold
-    text-shadow-lg/100  text-shadow-green-900
-    ml-5"
-    >Leaf</h1>
-
-    <h2 className="text-2xl/normal italic font-serif font-semibold
-    text-shadow-lg/20 text-shadow
-    ml-2 mt-4"
-    >~ <span id="breeze">Breeeze</span> Through The Classics ~  </h2>
-
 
     </>
   )
 };
 
-export default App
+export default App;
--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <navBar />
     <App />
   </StrictMode>,
 )
