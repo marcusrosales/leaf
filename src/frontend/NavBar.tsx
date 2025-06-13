819ddf000dc982de54eb4065bbd6842883550f6b
marcusrosales <marcusrosalesofficial5@gmail.com>
2025-06-13 15:32:54 -0500

    added market in skeleton

---
+++ src/frontend/NavBar.tsx
@@ -0,0 +1,29 @@
+
+
+
+function NavBar() {
+
+    return(
+        <>
+            <nav>
+                <div className="flex">
+                
+                
+                <h1 className="text-5xl navTexts border-4 text-center w-40 h-16" 
+                >HOME</h1>
+
+                <h1 className="text-5xl navTexts border-4 text-center w-50 h-16"
+                >MARKET</h1>
+                
+                
+                </div>
+            </nav>
+
+
+        </>
+    )
+}
+
+
+
+export default NavBar;
