f78989ea039dc8dfdcfe2ec05c96c88af64ffc42
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-14 18:14:22 -0500

    implimented navigate function into buttons

--- src/frontend/NavBar.tsx
+++ src/frontend/NavBar.tsx
@@ -2,19 +2,21 @@ import  {useNavigate} from 'react-router-dom'
 
 function NavBar() {
 
+  const navigate = useNavigate();
+
   return (
     <nav>
       <ul className="flex">
         
         <button className="text-5xl navTexts border-3 text-center w-58 italic font-serif font-semibold"
-                onClick={()=>console.log('test HOME button')}>HOME</button>
+                onClick={()=>useNavigate('/test')}>HOME</button>
 
         <button className="text-5xl navTexts border-3 text-center w-58 italic font-serif font-semibold"
-                onClick={()=>console.log('test SHELF button')}
+                onClick={()=>useNavigate('/test')}
                 >SHELF</button>
 
         <button className="text-5xl navTexts border-3 text-center w-58 italic font-serif font-semibold"
-                onClick={()=>console.log('test market button')}
+                onClick={()=>useNavigate('/test')}
                 >MARKET</button>
 
       </ul>
