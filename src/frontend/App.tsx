a1e6c8de0b2d00eca7c02cff6a2fb7b1d80feeb8
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-14 18:17:12 -0500

    added nav bar component into app

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,9 +1,13 @@
+import NavBar from "./NavBar";
+
+
 
 function App() {
 
   return (
-    <>
 
+    <>
+      <NavBar />
     <div>
       <h1 className=" text-9xl/normal italic font-serif font-extrabold
       text-shadow-lg/100  text-shadow-green-900
--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import NavBar from './NavBar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NavBar /> 
     <App />
   </StrictMode>,
 )
