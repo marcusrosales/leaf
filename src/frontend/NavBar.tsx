ecee4590db2d9d986f53813895c794e7ad3b5f24
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-14 19:19:03 -0500

    need to fix border in css

--- src/frontend/NavBar.tsx
+++ src/frontend/NavBar.tsx
@@ -5,18 +5,19 @@ function NavBar() {
   //const navigate = useNavigate();
 
   return (
-    <nav>
-      <ul className="flex">
+    <nav className="border-4 p-4 navBadCustoms">
+      
+      <ul className="flex justify-center gap-50">
         
-        <button className="text-5xl navTexts border-3 text-center w-58 italic font-serif font-semibold"
+        <button className="text-5xl navTexts text-center w-58 italic font-serif font-semibold"
                 //onClick={()=>console.log('test HOME button')}
                 >HOME</button>
 
-        <button className="text-5xl navTexts border-3 text-center w-58 italic font-serif font-semibold"
+        <button className="text-5xl navTexts text-center w-58 italic font-serif font-semibold"
                 //onClick={()=>useNavigate('/test')}
                 >SHELF</button>
 
-        <button className="text-5xl navTexts border-3 text-center w-58 italic font-serif font-semibold"
+        <button className="text-5xl navTexts text-center w-58 italic font-serif font-semibold"
                 //onClick={()=>console.log('test market button')}
                 >MARKET</button>
 
