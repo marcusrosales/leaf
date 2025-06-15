c3a3af587a242a788afe9db5a1fcdaf07aff1f9e
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-14 22:09:54 -0500

    fixed issues and added features

--- src/frontend/NavBar.tsx
+++ src/frontend/NavBar.tsx
@@ -1,32 +1,50 @@
-//import  {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
+
 
 function NavBar() {
 
-  //const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <nav className="border-4 p-4 navBadCustoms">
 
+    <nav className="border-4 p-4 navBadCustoms">
       <ul className="flex justify-center gap-50">
+
+
+        <div className="flex justify-center">
         
+        <img src="src/frontend/assets/Home.svg" className="size-13 -mr-3 "></img>
         <button className="text-5xl navTexts text-center w-58 italic font-serif font-semibold
                 text-shadow-lg/100  text-shadow-green-900"
                 //onClick={()=>console.log('test HOME button')}
                 >HOME</button>
+        </div>
 
+
+        <div className="flex justify-center">
+        
+        <img src="src/frontend/assets/Shelf.svg" className="size-13 -mr-3 "></img>
         <button className="text-5xl navTexts text-center w-58 italic font-serif font-semibold
                 text-shadow-lg/100  text-shadow-green-900"
                 //onClick={()=>useNavigate('/test')}
                 >SHELF</button>
+        </div>
 
+        <div className="flex justify-center">
+
+        <img src="src/frontend/assets/Market.svg" className="size-13 mr-3 "></img>
         <button className="text-5xl navTexts text-center w-58 italic font-serif font-semibold
                 text-shadow-lg/100  text-shadow-green-900"
                 //onClick={()=>console.log('test market button')}
-                >MARKET</button>
+                >MARKET</button> 
+        </div>
 
-      </ul>
+      </ul>    
     </nav>
-  );
+
+);
 }
 
 
