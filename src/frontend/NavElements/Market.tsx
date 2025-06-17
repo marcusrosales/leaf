93f72660c8168aa51e5be2a827b2dee95cb18bab
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-17 18:39:49 -0500

    working on inputs

--- src/frontend/NavElements/Market.tsx
+++ src/frontend/NavElements/Market.tsx
@@ -4,10 +4,28 @@ function Market() {
     return(
     <>
     <NavBar />
+
+      <div className="flex gap-100">
+    
     <h1 className="text-9xl/normal italic font-serif font-extrabold
       text-shadow-lg/100  text-shadow-green-900
       ml-5"
-    >my market</h1>    
+    >my market</h1> 
+
+      <div className="mt-10">
+
+
+      <input className="w-100 h-25 text-5xl"
+      ></input>
+
+
+      <button
+      >search!</button>
+
+      </div>
+
+      </div>
+
     </>)
 }
 
