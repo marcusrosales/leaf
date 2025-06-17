cd90c570c8d367c2a8ec905575ec3df6278a850e
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-16 21:56:07 -0500

    working with larger api example for apis probably gonna link up postgress supabase to make storing easier

--- src/frontend/NavElements/Home/testAlgo.js
+++ src/frontend/NavElements/Home/testAlgo.js
@@ -9,11 +9,59 @@
 let recents = []
 
 
-let ApiTest = { vehicle:"Car", 
-                    wheelCount:'Four', 
-                    brandName:'Dodge',
-        }
-
+let ApiTestResult = {
+  book1: {
+    bookID: "1",
+    bookName: "The Hobbit",
+    bookDetails: {
+      bookDescription: "A Good Book About Adventure!",
+      bookGenre: "Adventure",
+      pages: 300,
+      publisher: "Penguin Clubhouse",
+    },
+  },
+  book2: {
+    bookID: "2",
+    bookName: "1984",
+    bookDetails: {
+      bookDescription: "A dystopian novel about totalitarianism.",
+      bookGenre: "Sci-Fi",
+      pages: 328,
+      publisher: "Secker & Warburg",
+    },
+  },
+  book3: {
+    bookID: "3",
+    bookName: "To Kill a Mockingbird",
+    bookDetails: {
+      bookDescription: "A novel about racial injustice.",
+      bookGenre: "Fiction",
+      pages: 281,
+      publisher: "J.B. Lippincott & Co.",
+    },
+  },
+  book4: {
+    bookID: "4",
+    bookName: "Pride and Prejudice",
+    bookDetails: {
+      bookDescription: "A classic romance novel.",
+      bookGenre: "Romance",
+      pages: 432,
+      publisher: "T. Egerton",
+    },
+  },
+  book5: {
+    bookID: "5",
+    bookName: "The Great Gatsby",
+    bookDetails: {
+      bookDescription: "A story of the American dream gone wrong.",
+      bookGenre: "Classic",
+      pages: 218,
+      publisher: "Charles Scribner's Sons",
+    },
+  },
+};
+console.log(ApiTestResult.bookName)
 
 
 
