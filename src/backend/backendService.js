fe2375d85c9a53e00f90c75e2ba4b0babf6dab5b
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-17 17:49:27 -0500

    making fetch function and running tests

--- src/backend/backendService.js
+++ src/backend/backendService.js
@@ -2,5 +2,20 @@
 
 
 // Setup for inital API calling
-let url = 'https://openlibrary.org/search.json
+let url = 'https://openlibrary.org/search.json'
+
+
+async function fetchAllBooks(){
+    
+    let response = await fetch(url)
+    let data =  await response.json
+
+
+    console.log("function ran!")
+    console.log(await data)
+}
+
+
+fetchAllBooks()
+
 
