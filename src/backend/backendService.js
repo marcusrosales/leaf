7c958829a142a1f28f2ec3c04203d9f3c6ed1609
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-18 14:02:00 -0500

    blocked out some old backend code just experimenting best practices

--- src/backend/backendService.js
+++ src/backend/backendService.js
@@ -1,20 +1,16 @@
 // Setup for Backend
 
 
-
-
-
-
-
 // Function that handles the api call and returns result 
-async function fetchAllBooks(usersBookRequestARG){
 
-    let userBookRequest = usersBookRequestARG
-    let url = `https://openlibrary.org/search.json?q=${usersBookRequestARG}&limit=1`
 
-    let response = await fetch(url)
-    let data =  await response.json()
+// async function fetchAllBooks(usersBookRequestARG){
 
-    return(await data)
-}
+//     let userBookRequest = usersBookRequestARG
+//     let url = `https://openlibrary.org/search.json?q=${usersBookRequestARG}&limit=1`
 
+//     let response = await fetch(url)
+//     let data =  await response.json()
+
+//     return(await data)
+// }
--- src/frontend/NavElements/Market.tsx
+++ src/frontend/NavElements/Market.tsx
@@ -5,7 +5,6 @@ import { useState } from "react"
 
 
 
-
 function Market() {
   let [value, setValue] = useState('')
   
@@ -21,15 +20,13 @@ function Market() {
   async function clickFunc(value){
     console.log("code ran")
     testFetchBook(value)
-
   }
 
 
+
   return(
-    
     <>
     <NavBar />
-
       <div className="flex gap-50">
     
       <h1 className="text-9xl/normal italic font-serif font-extrabold
@@ -37,8 +34,8 @@ function Market() {
       ml-5"
      >my market</h1> 
 
-      <div className=" flex mt-15 gap-3 ">
 
+      <div className=" flex mt-15 gap-3 ">
 
       <input placeholder="Enter Book Name" className="w-170 h-23 text-5xl/normal italic font-serif font-extralight border-4 rounded-3xl
         p-5"
@@ -47,18 +44,19 @@ function Market() {
           setValue(e.target.value)
         }></input>
 
-
-
-
       <button className="w-50 h-23 text-5xl/normal italic font-serif font-extralight border-4 rounded-3xl bg-green-300
       text-shadow-lg/100 text-white border-black"
 
       onClick={()=>{clickFunc(value)}}
+
       >Search</button>
-
         </div>
-      </div>
 
+
+
+      </div>
+      
+      <h1></h1>
     </>)
     }
 
