f222ab6394ee9ca9ab65217ebbb8cdfef2659910
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-17 23:04:40 -0500

    implementing express json parsing into backend server

--- src/backend/backendService.js
+++ src/backend/backendService.js
@@ -3,21 +3,29 @@
 
 // setup for server
 import express from 'express';
+import cors from 'cors'
 let app = express();
+
+app.use(cors());
+app.use(express.json());
 
 
 app.post('/', async (req,res)=>{
     const data = await fetchAllBooks()
     console.log("test") 
-    res.send('hello')
+    res.send('hello',string)
 
-})
+});
 
+
+app.get(3000,(req,res)=>{
+    
+});
 
 app.listen(3000,()=>{
     console.log("listening on port 3k")
     console.log('test')
-})
+});
 
 
 
