be25406bb30df34851a28be34af1a984ea9cd199
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-17 19:00:09 -0500

    added and styled input methods on market tab just need to write working code for it

--- src/frontend/NavElements/Market.tsx
+++ src/frontend/NavElements/Market.tsx
@@ -5,22 +5,24 @@ function Market() {
     <>
     <NavBar />
 
-      <div className="flex gap-100">
+      <div className="flex gap-50">
     
     <h1 className="text-9xl/normal italic font-serif font-extrabold
       text-shadow-lg/100  text-shadow-green-900
       ml-5"
     >my market</h1> 
 
-      <div className="mt-10">
+      <div className=" flex mt-15 gap-3 ">
 
 
-      <input className="w-100 h-25 text-5xl"
+      <input placeholder="Enter Book Name" className="w-170 h-23 text-5xl/normal italic font-serif font-extralight border-4 rounded-3xl
+        p-5"
       ></input>
 
 
-      <button
-      >search!</button>
+      <button className="w-50 h-23 text-5xl/normal italic font-serif font-extralight border-4 rounded-3xl bg-green-300
+      text-shadow-lg/100 text-white border-black"
+      >Search</button>
 
       </div>
 
