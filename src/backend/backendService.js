8f0af48fb8fc82caf02200abe6542f0904cb0282
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-17 22:53:51 -0500

    set up backend server

--- src/backend/backendService.js
+++ src/backend/backendService.js
@@ -1,22 +1,26 @@
 // Setup for Backend
 
 
-
-
-
-
+// setup for server
 import express from 'express';
 let app = express();
+
+
+app.listen(3000,()=>{
+    console.log("listening on port 3k")
+})
+
+
+
 
 
 
 
 // Function that handles the api call and returns result 
-export async function fetchAllBooks(usersBookRequestARG){
-    
+async function fetchAllBooks(usersBookRequestARG){
+
     let userBookRequest = usersBookRequestARG
     let url = `https://openlibrary.org/search.json?q=${usersBookRequestARG}&limit=1`
-
 
     let response = await fetch(url)
     let data =  await response.json()
